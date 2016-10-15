633d8de68fe0d5bbae465a92a2e79b7b8886fe94
kaileeagray <kaileeagray@gmail.com>
2016-10-15 15:51:08 -0500

    End of tutorail 4

---
+++ NOTES.md
@@ -0,0 +1,4 @@
+# Notes from Tutorial 4
+https://www.youtube.com/watch?v=LYKRkHSLE2E&index=4&list=PLoYCgNOIyGABdI2V8I_SWo22tFpgh2s6_
+
++ jquery is made to be a DOM traversal tool -- document object model
--- index2.js
+++ index2.js
@@ -1,4 +1,53 @@
 $(function() {
 
+  // $('li').on('click', function() {
+  //     $(this).next().remove();
+  //     $(this).siblings().addClass('special');
+  // });
+  //
+  // $('li').on('mouseover', function() {
+  //     $(this).removeClass('special');
+  // });
+  //
+  // $('li').on('click', function() {
+  //     $(this).closest('.list').addClass('special');
+  // });
+
+  // $('.list').on('click', function() {
+  //   $(this).find('li').toggleClass('special');
+  // });
+  //
+  // $('.list').on('click', function() {
+  //   $(this).find('li').filter(':first').addClass('special');
+  // });
+
+  // $('.list').on('click', function() {
+  //   $(this).find('li').filter('.special').remove();
+  // });
+  //
+  // $('.special').on('click', function() {
+  //     $(this).hide();
+  //     if ($(this).is('.special')) {
+  //     alert('special');
+  //   }
+  // });
+
+  // $('li').on('click', function() {
+  //     $(this).hide();
+  //     if (!$(this).is('.special')) {
+  //     alert('not special');
+  //   }
+  // });
+
+  // $('li').on('click', function() {
+  //   $(this).hide();
+  // });
+
+  $('.sublist li').on('click', function() {
+    if ($(this).parent().is('.sublist')) {
+      $(this).hide();
+    }
+  });
+
 
 });
