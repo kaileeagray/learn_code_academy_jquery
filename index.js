f3da241f023262a85b0b4be7019dd12f0cc30ad1
kaileeagray <kaileeagray@gmail.com>
2016-10-15 14:59:32 -0500

    Begin tutorial 4 in index 2

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-$(document).ready(function() {
+$(function() {
   $('#btn1').html('my button');
-
-  $('#panel1').on('click', function() {
-    $('#panel1').fadeToggle(200);
-  });
-
-  $('#btn2').on('click', function() {
-    $('#panel2').fadeToggle(200);
-  });
-
-  $('#btn3').on('click', function() {
-    $('#panel3').fadeToggle(200);
-  });
-
-  $('#btn4').on('click', function() {
-    $('#panel4').fadeToggle(200);
-  });
 
   $('#panel1').on('mouseover', function() {
     $('#panel1').fadeToggle(200);
@@ -25,6 +9,12 @@ $(document).ready(function() {
     $('#panel1 .panel-body').html('my new panel content');
   });
 
-});
+  var content = "My New Awesome Content";
 
-// same as $(function() {});
+  $('.panel-button').on('click', function() {
+    var panelId = $(this).attr('data-panelid');
+    $('#' + panelId).fadeToggle(200);
+    $('#' + panelId + ' .panel-body').html('');
+  });
+
+});
