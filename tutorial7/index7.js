a524eaf845bdb421db4e550378c3088ccfd6ee3a
kaileeagray <kaileeagray@gmail.com>
2016-10-15 20:20:28 -0500

    Finished tutorial 8. API not avail. Faked it

--- tutorial7/index7.js
+++ tutorial7/index7.js
@@ -1,9 +1,49 @@
 $(function() {
   var $orders = $('#orders');
+  var $name = $('#name');
+  var $drink = $('#drink');
+
+  function addOrder(order) {
+    $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
+  };
 
   var orders = [{"id":1, "name":"Will", "drink":"Americano w/ Creme"}, {"id":2, "name":"Laura", "drink":"Vanilla Latte"}];
   $.each(orders, function(i, order) {
-    $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
+      addOrder(order);
+  });
+
+  $.ajax({
+    type: 'GET',
+    url: '/api/orders',
+    success: function(orders) {
+      $.each(orders, function(i, order) {
+        $orders.append('<li>name: ' + order.name + ', drink: ' + order.drink + '</li>');
+      });
+    },
+    error: function() {
+      alert('error loading orders');
+    }
+  });
+
+  $('#add-order').on('click', function() {
+
+    var order = {
+      name: $name.val(),
+      drink: $drink.val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/orders'
+      data: order,
+      success: function(newOrder) {
+          addOrder(newOrder);
+        error: function() {
+          alert('error saving order');
+        }
+      }
+    });
+
   });
 
 });
